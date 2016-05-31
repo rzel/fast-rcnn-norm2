e3937ff323cc5078d5a7ce8efde9f78f0b744f2d
Xiaolong Wang <xiaolonw@cs.cmu.edu>
2016-05-30 23:32:58 -0400

    test

--- tools/test_net2.py
+++ tools/test_net2.py
@@ -10,7 +10,7 @@
 """Test a Fast R-CNN network on an image database."""
 
 import _init_paths
-from fast_rcnn.test import test_net
+from fast_rcnn.test import test_net2
 from fast_rcnn.config import cfg, cfg_from_file
 from datasets.factory import get_imdb
 import caffe
